06ad8adac0f3dc9a4ef2541f9d383f53ead397f1
DuskyByte <samuel.j.smith.thrasher@gmail.com>
2018-10-25 08:40:56 -0700

    feat: Center on defined neighborhood

--- src/GoogleMaps.js
+++ src/GoogleMaps.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 
 class GoogleMaps extends Component {
-  getGoogleMapsAPI() {
-    if (!this.googleMapsAPI) {
-      this.googleMapsAPI = new Promise((resolve) => {
-        window.loadGoogleMap = () => {
+  loadGoogleMapsAPI() { //Loads the Google Maps API
+    if (!this.googleMapsAPI) { //Checks if Google Maps API does not already exsist
+      this.googleMapsAPI = new Promise((resolve) => { //Resolves and creates the Google Maps API
+        window.initializeGoogleMap = () => { //Creates function to initiralize the map
           resolve(window.google)
-          delete window.loadGoogleMap
+          delete window.initializeGoogleMap
         }
+        //Creates script to initialize the map once Google Maps API is loaded.
         const script = document.createElement("script")
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${this.props.APIKey}&callback=loadGoogleMap`
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${this.props.APIKey}&callback=initializeGoogleMap`
         script.async = true
         document.body.appendChild(script)
       })
@@ -19,16 +20,28 @@ class GoogleMaps extends Component {
   }
 
   componentWillMount() {
-    this.getGoogleMapsAPI()
+    //Prepares Google Maps API for use before Mounting component
+    this.loadGoogleMapsAPI()
   }
 
   componentDidMount() {
-    this.getGoogleMapsAPI().then((google) => {
-      const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 45.5108876, lng: -122.5028639},
-        zoom: 12
+    //Creates map object for use once component Mounts
+    this.loadGoogleMapsAPI().then((google) => {
+      const neighborhood = 'Gresham, OR'
+      const geocoder = new google.maps.Geocoder()
+      geocoder.geocode({
+        'address': neighborhood
+      }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          const mapOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng())
+          }
+          const map = new google.maps.Map(
+            document.getElementById('map'), mapOptions)
+          return map
+        }
       })
-      return map
     })
   }
 
